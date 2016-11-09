84f2e7f5320c283a711e6a1b02325bbaa236ce86
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-09 13:46:55 +0000

    Add details usercontrol

---
+++ RustCalc/RustCalc/UserControl/CraftingDetails.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MyLibrary.SelectPanel;
+
+namespace RustCalc.UserControl
+{
+    /// <summary>
+    /// Interaction logic for CraftingDetails.xaml
+    /// </summary>
+    public partial class CraftingDetails
+    {
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.Register("Item", 
+                typeof(IPanelItem), 
+                typeof(CraftingDetails), 
+                new PropertyMetadata(null));
+        
+        public CraftingDetails()
+        {
+            InitializeComponent();
+        }
+
+        public IPanelItem Item
+        {
+            get { return (IPanelItem)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+    }
+}
