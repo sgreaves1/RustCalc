9054fbd2a90823e8007aa27ca2ae3f6765cd4f8b
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-09 13:12:05 +0000

    add crafting items model

--- RustCalc/RustCalc/ViewModel/MainWindowViewModel.cs
+++ RustCalc/RustCalc/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RustCalc.Model;
 
 namespace RustCalc.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        private ObservableCollection<CraftingItemModel> _craftableItems = new ObservableCollection<CraftingItemModel>();
+
+        public ObservableCollection<CraftingItemModel> CraftableItem
+        {
+            get { return _craftableItems; }
+            set { _craftableItems = value; }
+        }  
     }
 }
