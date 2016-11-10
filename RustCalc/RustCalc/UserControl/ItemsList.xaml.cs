04b22d1b652394533c90609a6a924afa6f340298
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-11-10 10:08:35 +0000

    Add user control to display all items belonging to components ect

---
+++ RustCalc/RustCalc/UserControl/ItemsList.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace RustCalc.UserControl
+{
+    /// <summary>
+    /// Interaction logic for ItemsList.xaml
+    /// </summary>
+    public partial class ItemsList 
+    {
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ItemsList), new PropertyMetadata("Title:"));
+        
+        public ItemsList()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
